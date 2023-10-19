50c2bc9b0c3fbc1524cd0f2b11b83d0d159ccd35
Marbal27 <Marbel27@yandex.ru>
2023-10-19 20:38:31 +0500

    фрагмент1

--- app/src/main/java/com/example/lr5_/MainActivity.java
+++ app/src/main/java/com/example/lr5_/MainActivity.java
@@ -6,11 +6,18 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.example.lr5_.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_form;
+    EditText FIO,Email;
     ActivityMainBinding binding;
 
     @Override
@@ -25,28 +32,31 @@ public class MainActivity extends AppCompatActivity {
             int menuId = item.getItemId();
             if(menuId == R.id.text){
                 replaceFragment(new TextFragment());
-//              break;
+
             } else if (menuId == R.id.question) {
                 replaceFragment(new QuestionFragment());
-//              break;
+
             } else if (menuId == R.id.bell) {
                 replaceFragment(new BellFragment());
-//              break;
+
             }
 
-//            switch (menuId){
-//                case R.id.text:
-//                    replaceFragment(new TextFragment());
-//                    break;
-//                case R.id.question:
-//                    replaceFragment(new QuestionFragment());
-//                    break;
-//                case R.id.bell:
-//                    replaceFragment(new BellFragment());
-//                    break;
-//            }
-
             return true;
+        });
+
+        setContentView(R.layout.activity_main);
+        FIO = findViewById(R.id.editText_FIO);
+        Email = findViewById(R.id.editTextEmail);
+        btn_form = findViewById(R.id.btn_form);
+
+        btn_form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FIO.setText("");
+                Email.setText("");
+                Toast myToast= Toast.makeText(getApplicationContext(),"УСПЕХ!",Toast.LENGTH_SHORT);
+                myToast.show();
+            }
         });
     }
 
--- app/src/main/java/com/example/lr5_/TextFragment.java
+++ app/src/main/java/com/example/lr5_/TextFragment.java
@@ -11,54 +11,16 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TextFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TextFragment extends Fragment {
 
     EditText FIO,Email;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TextFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TextFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TextFragment newInstance(String param1, String param2) {
-        TextFragment fragment = new TextFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -71,10 +33,10 @@ public class TextFragment extends Fragment {
 
 
 
-    public void form_btn(View view) {
-        FIO.setText("");
-        Email.setText("");
-//        Toast myToast= Toast.makeText(getApplicationContext(),"УСПЕХ!",Toast.LENGTH_SHORT);
-//        myToast.show();
-    }
+//    public void form_btn(View view) {
+//        FIO.setText("");
+//        Email.setText("");
+////        Toast myToast= Toast.makeText(getApplicationContext(),"УСПЕХ!",Toast.LENGTH_SHORT);
+////        myToast.show();
+//    }
 }
